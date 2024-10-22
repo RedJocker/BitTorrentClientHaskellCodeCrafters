3851c517111e404ed3247135217f85f3be805cf8
redjocker <mauricio.guaruja@gmail.com>
2024-10-21 23:10:08 -0300

    Feat: info command from torrent file

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,41 @@ import Data.Char (isDigit)
 import Data.Tuple (swap)
 import System.Environment
 import System.Exit
+import System.IO
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString as BS
 import qualified Data.Map.Strict as Map
 import Parser
+
+doDecode :: String -> IO ()
+doDecode encodedValue = do
+  let decodedValue = Parser.decodeBencodedValue(B.pack encodedValue)                   
+  let jsonValue = encode decodedValue
+  LB.putStr jsonValue
+  putStr "\n"
+
+doInfo :: String -> IO ()
+doInfo filePath = do
+  
+  handle <- openBinaryFile filePath ReadMode
+  fileContent <- B.hGetContents handle
+  let decodedValue = Parser.decodeBencodedValue fileContent
+ 
+  case decodedValue of
+    BencDict dict -> do
+      let trackerUrl = (Map.!) dict "announce"
+      let infoValue = (Map.!) dict "info"
+      case (trackerUrl, infoValue) of
+        (BencString str, BencDict infoDict) -> do
+          let lengthValue = (Map.!) infoDict "length"
+          case lengthValue of
+            (BencInteger len) -> do
+              putStrLn $ "Tracker URL: " ++ (B.unpack str)
+              putStrLn $ "Length: " ++ (show len)
+            _ -> do return ()
+        _ -> do return ()
+    _ -> do return ()
 
 main :: IO ()
 main = do
@@ -22,12 +53,6 @@ main = do
 
     let command = args !! 0
     case command of
-        "decode" -> do
-          -- You can use print statements as follows for debugging, they'll be visible when running tests.           
-            let encodedValue = args !! 1
-            let decodedValue = Parser.decodeBencodedValue(B.pack encodedValue)
-            
-            let jsonValue = encode decodedValue
-            LB.putStr jsonValue
-            putStr "\n"
+        "decode" -> doDecode (args !! 1)
+        "info" -> doInfo (args !! 1) 
         _ -> putStrLn $ "Unknown command: " ++ command
