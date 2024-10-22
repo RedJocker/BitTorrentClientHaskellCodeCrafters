4925edf93da2f2ec16a68bd262a586269ec32104
redjocker <mauricio.guaruja@gmail.com>
2024-10-22 13:53:10 -0300

    Feat: info hash

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE OverloadedStrings #-}    
+{-# LANGUAGE OverloadedStrings #-}
 import Data.Aeson
 import Data.ByteString.Char8 (ByteString)
 import Data.Char (isDigit)
@@ -7,26 +7,31 @@ import Data.Tuple (swap)
 import System.Environment
 import System.Exit
 import System.IO
+import Control.Monad (when)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as LB
 import qualified Data.ByteString as BS
 import qualified Data.Map.Strict as Map
 import Parser
+import Crypto.Hash as Hash
+
+-- use the following command to create a language server for this ghc version
+-- ghcup compile hls --version 2.6.0.0 --ghc 9.4.6
 
 doDecode :: String -> IO ()
 doDecode encodedValue = do
-  let decodedValue = Parser.decodeBencodedValue(B.pack encodedValue)                   
+  let decodedValue = Parser.decodeBencodedValue (B.pack encodedValue)
   let jsonValue = encode decodedValue
   LB.putStr jsonValue
   putStr "\n"
 
 doInfo :: String -> IO ()
 doInfo filePath = do
-  
+
   handle <- openBinaryFile filePath ReadMode
   fileContent <- B.hGetContents handle
   let decodedValue = Parser.decodeBencodedValue fileContent
- 
+
   case decodedValue of
     BencDict dict -> do
       let trackerUrl = (Map.!) dict "announce"
@@ -34,25 +39,27 @@ doInfo filePath = do
       case (trackerUrl, infoValue) of
         (BencString str, BencDict infoDict) -> do
           let lengthValue = (Map.!) infoDict "length"
+          let infoBencoded = bencodeValue infoValue
           case lengthValue of
             (BencInteger len) -> do
-              putStrLn $ "Tracker URL: " ++ (B.unpack str)
-              putStrLn $ "Length: " ++ (show len)
-            _ -> do return ()
-        _ -> do return ()
-    _ -> do return ()
+              putStrLn $ "Tracker URL: " ++ B.unpack str
+              putStrLn $ "Length: " ++ show len
+              let sha1 = (Hash.hash infoBencoded :: Hash.Digest Hash.SHA1)
+              putStr "Info Hash: "
+              print sha1
+            _otherwise -> do return ()
+        _otherwise -> do return ()
+    _otherwise -> do return ()
 
 main :: IO ()
 main = do
     args <- getArgs
-    if length args < 2
-        then do 
+    when (length args < 2) $ do
             putStrLn "Usage: your_bittorrent.sh <command> <args>"
             exitWith (ExitFailure 1)
-        else return ()
 
     let command = args !! 0
     case command of
         "decode" -> doDecode (args !! 1)
-        "info" -> doInfo (args !! 1) 
+        "info" -> doInfo (args !! 1)
         _ -> putStrLn $ "Unknown command: " ++ command
--- app/Parser.hs
+++ app/Parser.hs
@@ -2,16 +2,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Parser (
-  Bencoded(BencString, BencInteger, BencList, BencDict), decodeBencodedValue
+  Bencoded(BencString, BencInteger, BencList, BencDict),
+  decodeBencodedValue,
+  bencodeValue
 ) where
 
 
 import Data.Aeson
 import Data.ByteString.Char8 (ByteString)
-import Data.Char (isDigit)
+import Data.Char (isDigit, ord)
 import Data.Tuple (swap)
 import System.Environment
 import System.Exit
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as LB
 import qualified Data.Map.Strict as Map
@@ -19,10 +22,10 @@ import qualified Data.Map.Strict as Map
 a |> f = f a
 
 data Bencoded =
-  BencString ByteString
-  | BencInteger Int
-  | BencList [Bencoded]
-  | BencDict (Map.Map String Bencoded)
+  BencString !ByteString
+  | BencInteger !Int
+  | BencList ![Bencoded]
+  | BencDict !(Map.Map String Bencoded)
   deriving (Show, Ord, Eq)
 
 instance ToJSON Bencoded where
@@ -74,13 +77,14 @@ parseBencodedDict encodedValue =
         | (==) "" str = Nothing
         | (==) 'e' (B.head str) = Just (B.tail str, reverse acc)
         | otherwise = parseKeyValue str
-                      >>= \(rest, key, value) -> parseDict ((key, value):acc) rest
-                                                 
+                      >>= \(rest, key, value) ->
+                            parseDict ((key, value):acc) rest
+
   in
     (B.stripPrefix "d" encodedValue)
     >>= (parseDict [])
     >>= (\(rest, assocLst) -> Just (rest, BencDict (Map.fromList assocLst)))
-  
+
 parseBencodedValue :: ByteString -> Maybe (ByteString, Bencoded)
 parseBencodedValue encodedValue
   | isDigit (B.head encodedValue) =
@@ -97,4 +101,37 @@ decodeBencodedValue :: ByteString -> Bencoded
 decodeBencodedValue encodedValue =
   case parseBencodedValue encodedValue of
     Just ("", decoded) -> decoded
-    otherwise -> error $ "Unhandled encoded value: " ++ B.unpack encodedValue
+    _otherwise -> error $ "Unhandled encoded value: " ++ B.unpack encodedValue
+
+bencodeValue :: Bencoded -> ByteString
+bencodeValue (BencString str) =
+  let lenStr = (B.pack . show . B.length) str
+  in
+    lenStr
+    |> \s -> BS.append s (B.pack ":")
+    |> \s -> BS.append s str
+
+bencodeValue (BencInteger int) =
+  let intStr = (B.pack . show) int
+  in
+    intStr
+    |> BS.cons ((fromIntegral . ord) 'i')
+    |> \s -> BS.append s "e"
+
+bencodeValue (BencList lst) =
+   BS.concat (bencodeValue <$> lst)
+   |> BS.cons ((fromIntegral . ord) 'l')
+   |> \s -> BS.append s "e"
+
+bencodeValue (BencDict dict) =
+   let assoc = Map.toAscList dict
+       entryAsBencStr (key, value) =
+         let keyLen = (show . length) key
+             keyBenc = B.pack (keyLen ++ ":" ++ key)
+             valueBenc = bencodeValue value
+         in
+           B.append keyBenc valueBenc
+   in
+   BS.concat (entryAsBencStr <$> assoc)
+   |> BS.cons ((fromIntegral . ord) 'd')
+   |> \s -> BS.append s "e"
